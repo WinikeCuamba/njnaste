42b0c2b995df8a8cc079c55aa3a8063683deb79f
WinikeCuamba <WinikeCuamba@github.com>
2023-12-26 13:22:08 +0200

    create repository

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let mut frase_usuario = "Nomeo";
+    let mut map  = [];
+    let mut counter = 0;
+    for leter in frase_usuario.chars().collect::<Vec<char>>(){
+        map
+    }
+
+    println!("{:?}" ,map);
+
+
+
+    
+
+
+}
